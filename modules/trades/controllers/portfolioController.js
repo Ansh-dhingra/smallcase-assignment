7b9432487e26fde7067e96207ab413b35fb72062
Ansh Dhingra <anshdhingra33@gmail.com>
2021-08-08 23:18:44 +0530

    Consider quanity greater than 0 only

--- modules/trades/controllers/portfolioController.js
+++ modules/trades/controllers/portfolioController.js
@@ -8,7 +8,9 @@ const tradeService      = require("../services/tradeService");
 */
 exports.getReturns = async (req, res) => {
     try {
-        const portfolioResult = await portfolioModel.find({});
+        const portfolioResult = await portfolioModel.find({
+            shareQuantity : { $gt : 0 }
+        });
         let netReturn = 0;
         // SUM(currentPrice[security] * shareQuantity)
         portfolioResult.map((portfolio) => netReturn += (constants.CURRENT_STOCK_PRICE - portfolio.avgBuyPrice) * portfolio.shareQuantity )
@@ -31,7 +33,13 @@ exports.getReturns = async (req, res) => {
 exports.getPortfolio = async (req, res) => {
     try {
         // Get all Security to display a consolidated Portfolio 
-        const portfolioResult = await portfolioModel.find({});
+        const portfolioResult = await portfolioModel.find({
+            $or : [{
+                shareQuantity : { $gt : 0 }
+            },{
+                avgBuyPrice : { $gt : 0}
+            }]
+        });
         !portfolioResult || !portfolioResult.length ? res.status(constants.notFoundHTTPCode).send({
             message : constants.RESPONSE_MESSAGES.NO_DATA_FOUND,
         }) : res.status(constants.successHTTPCode).send({
